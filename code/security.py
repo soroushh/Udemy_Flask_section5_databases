45a38545ec9d42bc811906931817ac13a4146699
student <sorush.kh68@gmail.com>
2019-07-03 15:56:51 +0100

    We are able to retrieve users from the database now.

--- code/security.py
+++ code/security.py
@@ -1,21 +1,10 @@
 from user import User
-users = [
-User(1,"soroush","1234")
-]
-
-username_mapping = {
-user.username:user for user in users
-}
-
-userid_mapping = {
-user.id:user for user in users
-}
 
 def authenticate(username, password):
-    user = username_mapping.get(username , None)
+    user = User.find_by_username(username)
     if user and user.password == password :
         return(user)
 
 def identity(payload):
     user_id = payload["identity"]
-    return userid_mapping.get(user_id , None)
+    return User.find_by_id(user_id)
--- test.py
+++ test.py
@@ -1,29 +1,15 @@
 import sqlite3
+
 connection = sqlite3.connect("data.db")
 cursor = connection.cursor()
-create_table = "CREATE TABLE people (name text, family text)"
-cursor.execute(create_table)
 
-insert_into = "INSERT INTO people VALUES (?, ?)"
+query = "CREATE TABLE users (id int, username text, password text)"
 
-name =("soroush", "khosravi")
+cursor.execute(query)
 
-cursor.execute(insert_into, name)
+inserting_query = "INSERT INTO users VALUES (?,?,?)"
 
-insert_into_directly = "INSERT INTO people VALUES ('farnaz', 'ostovari')"
-
-cursor.execute(insert_into_directly)
-
-names ={('kati', 'yazdani'),('rostam', 'khosravi')}
-
-cursor.executemany(insert_into, names)
-
-find_query = "SELECT * FROM people WHERE family='khosravi'"
-
-soroush = cursor.execute(find_query)
-
-for name in soroush:
-    print(name)
+cursor.execute(inserting_query, (1,"soroush", "khosravi"))
 
 connection.commit()
 
